c9c9ec90b8cf1825812eb3cd46ba59df9f725109
Graham Blevins <hgblevins@gmail.com>
2016-04-13 18:10:04 -0400

    initial commit

---
+++ Base.php
@@ -0,0 +1,20 @@
+<?php namespace Expresser\Setting;
+
+use Exception;
+
+abstract class Base extends \Expresser\Support\Fluent {
+
+  public $ID = 'option';
+
+  public static function register() {
+
+    static::registerSettings();
+
+    parent::register();
+  }
+
+  protected static function registerSettings() {
+
+    throw new Exception('A settings page must override registerSettings.');
+  }
+}
